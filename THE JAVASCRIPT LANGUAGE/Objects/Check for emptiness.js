7b9b52d6f682f81df9c4ffd5f61a05ae9452d497
OksanaSinyugina <111419697+OksanaSinyugina@users.noreply.github.com>
2022-09-26 21:04:22 +0300

    objects

---
+++ THE JAVASCRIPT LANGUAGE/Objects/Check for emptiness.js
@@ -0,0 +1,23 @@
+/*
+Write the function isEmpty(obj) which returns true if the object has no properties, false otherwise.
+
+Should work like that:
+let schedule = {};
+alert( isEmpty(schedule) ); // true
+schedule["8:30"] = "get up";
+alert( isEmpty(schedule) ); // false
+
+*/
+
+const isEmpty = (obj) => {
+    for (let key in obj) {
+        return false;
+    }
+    return true;
+}
+
+let schedule = {};
+console.log( isEmpty(schedule) ); // true
+
+schedule["8:30"] = "get up";
+console.log( isEmpty(schedule) ); // false
